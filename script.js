6a028cbb269d4075c8c9183f97a741a8086589cd
prasanna9926 <136099390+prasanna9926@users.noreply.github.com>
2023-06-12 23:08:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  
+  function fadeInSection() {
+    const sections = document.querySelectorAll('.section');
+    
+    sections.forEach((section) => {
+      if (isInViewport(section)) {
+        section.classList.add('visible');
+      }
+    });
+  }
+  
+  window.addEventListener('load', fadeInSection);
+  window.addEventListener('scroll', fadeInSection);
